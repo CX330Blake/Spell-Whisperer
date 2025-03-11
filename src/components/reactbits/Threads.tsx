5da593a14233bd7a56bab7c12c1584bf73b252b9
DESKTOP-RIVTRKI\user <timchen975@gmail.com>
2025-03-12 07:46:27 +0800

    Fix build error

--- src/components/reactbits/Threads.tsx
+++ src/components/reactbits/Threads.tsx
@@ -135,7 +135,7 @@ const Threads: React.FC<ThreadsProps> = ({
     ...rest
 }) => {
     const containerRef = useRef<HTMLDivElement>(null);
-    const animationFrameId = useRef<number>();
+    const animationFrameId = useRef<number>(0);
 
     useEffect(() => {
         if (!containerRef.current) return;
@@ -158,7 +158,7 @@ const Threads: React.FC<ThreadsProps> = ({
                     value: new Color(
                         gl.canvas.width,
                         gl.canvas.height,
-                        gl.canvas.width / gl.canvas.height
+                        gl.canvas.width / gl.canvas.height,
                     ),
                 },
                 uColor: { value: new Color(...color) },
