70b1621db1f29014475db5256baef56faa02fec3
CX330Blake <timchen975@gmail.com>
2025-03-12 15:36:59 +0800

    Fix github corner z-index

--- src/components/GitHubCorner.tsx
+++ src/components/GitHubCorner.tsx
@@ -16,7 +16,7 @@ export default function GitHubCorner() {
                 width="80"
                 height="80"
                 viewBox="0 0 250 250"
-                className="fixed top-0 right-0 border-0 fill-primary z-10"
+                className="fixed top-0 right-0 border-0 fill-primary z-50"
                 aria-hidden="true"
             >
                 <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z" />
