aafbcddc31918af0453e255f4b28e546be3a30ef
CX330Blake <timchen975@gmail.com>
2025-03-13 10:42:19 +0800

    Change chat UI and planned to add auth feature

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,53 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
-  ...
-})
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        // GoogleProvider({
+        //     clientId: process.env.GOOGLE_ID,
+        //     clientSecret: process.env.GOOGLE_SECRET,
+        // }),
+    ],
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            const isAllowedToSignIn = true;
+            if (isAllowedToSignIn) {
+                return true;
+            } else {
+                // Return false to display a default error message
+                return false;
+                // Or you can return a URL to redirect to:
+                // return '/unauthorized'
+            }
+        },
+        async redirect({ url, baseUrl }) {
+            // Allows relative callback URLs
+            if (url.startsWith("/")) return `${baseUrl}${url}`;
+            // Allows callback URLs on the same origin
+            else if (new URL(url).origin === baseUrl) return url;
+            return baseUrl;
+        },
+        async session({ session, user, token }) {
+            // Send properties to the client, like an access_token and user id from a provider.
+            session.accessToken = token.accessToken;
+            session.user.id = token.id;
 
-export { handler as GET, handler as POST }
+            return session;
+        },
+        async jwt({ token, user, account, profile, isNewUser }) {
+            // Persist the OAuth access_token and or the user id to the token right after signin
+            if (account) {
+                token.accessToken = account.access_token;
+                token.id = profile.id;
+            }
+            return token;
+        },
+    },
+});
+
+export { handler as GET, handler as POST };
--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,70 +1,102 @@
 "use client";
 
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { AiOutlineLoading } from "react-icons/ai";
+import { useState, useEffect, useRef } from "react";
 import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
-import { FaPaperPlane } from "react-icons/fa6";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { AiOutlineLoading } from "react-icons/ai";
+import { FaPaperPlane } from "react-icons/fa6";
+import { FaFlag } from "react-icons/fa";
 import { useLevel } from "@/contexts/LevelContext";
 import { Confetti } from "./Confetti";
 
+interface Message {
+    role: "user" | "bot";
+    message: string;
+}
+
 export default function Chat() {
-    const [userInput, setUserInput] = useState("");
-    const [resFromGrok, setResFromGrok] = useState("");
-    const { selectedLevel, setSelectedLevel } = useLevel();
+    const [conversation, setConversation] = useState<Message[]>([]);
+    const [input, setInput] = useState("");
+    const { selectedLevel } = useLevel();
     const [flag, setFlag] = useState("");
     const [flagBorderStyle, setFlagBorderStyle] = useState("border-primary");
     const [waiting, setWaiting] = useState(false);
+    const [levelName, setLevelName] = useState("");
 
-    const getResponseFromGrok = async () => {
+    const bottomRef = useRef<HTMLDivElement>(null);
+
+    const scrollToBottom = () => {
+        bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+    };
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [conversation]);
+
+    useEffect(() => {
+        async function getLevelName() {
+            if (selectedLevel) {
+                const data = await fetch("/api/challenge/get-info").then(
+                    (res) => res.json(),
+                );
+                setLevelName(data[selectedLevel][0]["name"]);
+            }
+        }
+        getLevelName();
+        setConversation([]);
+    }, [selectedLevel]);
+
+    const sendMessage = async () => {
+        if (!input) return;
+        // Add user input to conversation
+        setConversation((prev) => [...prev, { role: "user", message: input }]);
+        setWaiting(true);
         try {
-            setResFromGrok("Waiting for response...");
-            setUserInput("");
-            setWaiting(true);
             const res = await fetch("/api/challenge/chat", {
                 method: "POST",
-                body: JSON.stringify({
-                    input: userInput,
-                    level: selectedLevel,
-                }),
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ input, level: selectedLevel }),
             }).then((res) => res.json());
-            setResFromGrok(res.response);
-            setWaiting(false);
+
+            // Add response to conversation
+            setConversation((prev) => [
+                ...prev,
+                { role: "bot", message: res.response },
+            ]);
         } catch (error) {
-            setResFromGrok("Error occurred. Please try again.");
+            setConversation((prev) => [
+                ...prev,
+                { role: "bot", message: "Error occurred. Please try again." },
+            ]);
+        } finally {
             setWaiting(false);
+            setInput("");
         }
     };
 
     const checkFlag = async () => {
-        if (selectedLevel === undefined || selectedLevel === "") {
+        if (!selectedLevel) {
             setFlagBorderStyle("border-red-500 border-2");
             setFlag("❗ Choose a level!");
-
             setTimeout(() => {
                 setFlagBorderStyle("border-primary");
                 setFlag("");
             }, 1500);
-
             return;
         }
-
         const res = await fetch("/api/challenge/check-flag", {
             method: "POST",
-            body: JSON.stringify({ flag: flag, level: selectedLevel }),
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ flag, level: selectedLevel }),
         }).then((res) => res.json());
 
         if (res.correct) {
             Confetti();
             setFlagBorderStyle("border-green-500 border-2");
-            setFlag("✅ Congrats!");
         } else {
             setFlagBorderStyle("border-red-500 border-2");
             setFlag("❌ Wrong!");
         }
-
         setTimeout(() => {
             setFlagBorderStyle("border-primary");
             setFlag("");
@@ -73,63 +105,80 @@ export default function Chat() {
 
     return (
         <div className="flex flex-col items-center space-y-4 w-auto">
-            <div className="flex w-full gap-4 h-50">
-                {/* User input */}
-                <Textarea
-                    placeholder="Type your message here to chat with LLM"
-                    className="font-victor-mono w-1/2 h-full text-sm md:text-base lg:text-base border-primary resize-none bg-background"
-                    value={userInput}
-                    onChange={(e) => setUserInput(e.target.value)}
+            {/* Chat box */}
+            <div className="w-full p-4 border border-primary rounded-md h-50 overflow-y-auto bg-background font-victor-mono text-sm md:text-sm lg:text-sm">
+                {conversation.length === 0 ? (
+                    <div className="text-gray-500">No messages yet.</div>
+                ) : (
+                    conversation.map((msg, index) => (
+                        <div
+                            key={index}
+                            className={`my-2 space-y-2 ${
+                                msg.role === "user" ? "text-right" : "text-left"
+                            }`}
+                        >
+                            <div>{msg.role === "user" ? "You" : levelName}</div>
+                            <span
+                                className={`inline-block p-2 rounded bg-background border-primary border max-w-2/3`}
+                            >
+                                {msg.message}
+                            </span>
+                        </div>
+                    ))
+                )}
+                <div ref={bottomRef} />
+            </div>
+
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                }}
+                className="flex w-full max-w-2xl items-center space-x-2"
+            >
+                <Input
+                    type="text"
+                    placeholder="Type your message here"
+                    className={`w-3/4 bg-background border-primary font-victor-mono text-sm md:text-base lg:text-base`}
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
                 />
-                {/* Grok response */}
-                <Alert className="font-victor-mono w-1/2 h-full text-sm md:text-base lg:text-base border-primary resize-none overflow-y-auto">
-                    <AlertDescription className="text-primary">
-                        {resFromGrok ||
-                            "The response from the LLM will appear here"}
-                    </AlertDescription>
-                </Alert>
-            </div>
-            <br />
-            <div className="w-full flex-col space-y-4 md:w-1/3">
                 <Button
-                    className="font-playwrite w-full hover:cursor-pointer"
-                    onClick={getResponseFromGrok}
+                    type="submit"
+                    className="w-1/4 font-playwrite hover:cursor-pointer"
+                    onClick={sendMessage}
                 >
-                    <div className="flex justify-center space-x-2 items-center">
-                        {waiting ? (
-                            <AiOutlineLoading
-                                size={30}
-                                className="animate-spin"
-                            />
-                        ) : (
-                            <FaPaperPlane size={30} />
-                        )}
-                        <div>Send message</div>{" "}
-                    </div>
+                    {waiting ? (
+                        <AiOutlineLoading className="animate-spin" size={30} />
+                    ) : (
+                        <FaPaperPlane size={30} />
+                    )}
+                    Send
                 </Button>
-                <br />
-                <form
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        checkFlag();
-                    }}
-                    className="flex w-full items-center justify-stretch space-x-2"
+            </form>
+
+            {/* Flag submit */}
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    checkFlag();
+                }}
+                className="flex w-full max-w-2xl items-center space-x-2"
+            >
+                <Input
+                    type="text"
+                    placeholder="Submit the flag"
+                    className={`w-3/4 bg-background border-primary font-victor-mono ${flagBorderStyle} text-sm md:text-base lg:text-base`}
+                    value={flag}
+                    onChange={(e) => setFlag(e.target.value)}
+                />
+                <Button
+                    type="submit"
+                    className="w-1/4 font-playwrite hover:cursor-pointer"
                 >
-                    <Input
-                        type="text"
-                        placeholder="Submit the flag"
-                        className={`bg-background border-primary font-victor-mono w-3/4 ${flagBorderStyle} text-sm md:text-base lg:text-base`}
-                        value={flag}
-                        onChange={(e) => setFlag(e.target.value)}
-                    />
-                    <Button
-                        type="submit"
-                        className="font-playwrite hover:cursor-pointer w-1/4"
-                    >
-                        Submit
-                    </Button>
-                </form>
-            </div>
+                    <FaFlag size={30} />
+                    Submit
+                </Button>
+            </form>
         </div>
     );
 }
