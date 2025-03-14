d5779101aebd0c9f8f8d0a275c9ea177f65ffbb1
DESKTOP-RIVTRKI\user <timchen975@gmail.com>
2025-03-14 09:06:20 +0800

    Replace BlurText with SplitText component in Title and add SplitText implementation

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,13 +1,15 @@
 import BlurText from "@/components/reactbits/BlurText";
+import ShinyText from "./reactbits/ShinyText";
+import SplitText from "./reactbits/SplitText";
 
 export default function Title() {
     return (
         <span className="flex flex-col space-y-4 items-center">
             <span className="font-victor-mono text-2xl md:text-4xl lg:text-6xl">
-                <BlurText text="Spell Whisperer" />
+                <SplitText text="Spell Whisperer" />
             </span>
             <span className="font-playwrite justify-center text-sm md:text-base lg:text-xl">
-                <BlurText text="- Hello wizards, let's hack the LLM -" />
+                <SplitText text="- Hello wizards, let's hack the LLM -" />
             </span>
         </span>
     );
