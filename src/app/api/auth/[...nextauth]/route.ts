438b61fa2dd4159e030242118b5489c7db259651
CX330Blake <timchen975@gmail.com>
2025-03-21 19:04:51 +0800

    Fix authOption issue

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,43 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import GitHubProvider from "next-auth/providers/github";
-import { SupabaseAdapter } from "@auth/supabase-adapter";
-import { Adapter } from "next-auth/adapters";
-
-export const authOptions: NextAuthOptions = {
-    secret: process.env.NEXTAUTH_SECRET ?? "",
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-        }),
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID ?? "",
-            clientSecret: process.env.GITHUB_SECRET ?? "",
-        }),
-    ],
-    adapter: SupabaseAdapter({
-        url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
-        secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? "",
-    }) as Adapter,
-    callbacks: {
-        async session({ session, token }) {
-            if (session?.user) {
-                session.user.id = token.sub as string;
-            }
-            return session;
-        },
-        async jwt({ user, token }) {
-            if (user) {
-                token.uid = user.id;
-            }
-            return token;
-        },
-    },
-    session: {
-        strategy: "jwt",
-    },
-};
+import { authOptions } from "@/lib/authOptions";
+import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 
